88fb4457d04b347e0c112d32f09390519a92ac61
Adamhus77 <ahusar@zsp5kozuchow.pl>
2020-10-08 14:50:49 +0200

    Zmiany graficzne

--- js/main.js
+++ js/main.js
@@ -4,21 +4,21 @@ const mb = [                    /* produkt 0 */
         name:"Gigabyte B450 AORUS ELITE",
         price: 409,
         producent:"Gigabyte",
-        img: ""
+        img: "../img/mb0.png"
     },
     {
         id: 1,
         name:"MSI B450M PRO-VDH MAX",
         price: 329,
         producent:"MSI",
-        img: ""
+        img: "../img/mb1.png"
     },
     {
         id: 2,
         name:"MSI B450 TOMAHAWK MAX",
         price: 469,
         producent:"MSI",
-        img: ""
+        img: "../img/mb2.png"
     },
 ]
 const cpu = [                    /* produkt 1 */
@@ -27,19 +27,21 @@ const cpu = [                    /* produkt 1 */
         name:"AMD Ryzen 3 3100, 3.6GHz, 16 MB, BOX",
         price: 479,
         producent:"AMD",
-        img: ""
+        img: "../img/cpu0.png"
     },
     {
         id: 1,
         name:"Intel Core i5-9400F, 2.9GHz, 9 MB, BOX",
         price: 599,
-        producent:"Intel"
+        producent:"Intel",
+        img: "../img/cpu1.png"
     },
     {
         id: 2,
         name:"Intel Core i7-10700K, 3.8GHz, 16 MB, BOX",
         price: 1749,
-        producent:"Intel"
+        producent:"Intel",
+        img: "../img/cpu2.png"
     },
 ]
 const gpu = [                    /* produkt 2 */
@@ -47,19 +49,22 @@ const gpu = [                    /* produkt 2 */
         id: 0,
         name:"Gigabyte GeForce RTX 2060 OC 6GB GDDR6",
         price: 1399,
-        producent:"Gigabyte"
+        producent:"Gigabyte",
+        img: "../img/gpu0.png"
     },
     {
         id: 1,
         name:"MSI GeForce GTX 1660 SUPER Ventus XS OC 6GB GDDR6",
         price: 1021,
-        producent:"MSI"
+        producent:"MSI",
+        img: "../img/gpu1.png"
     },
     {
         id: 2,
         name:"Gainward GeForce RTX 2060 Phoenix GS 6GB GDDR6",
         price: 1599,
-        producent:"GeForce"
+        producent:"GeForce",
+        img: "../img/gpu2.png"
     },
 ]
 const ram = [                    /* produkt 3 */
@@ -67,19 +72,22 @@ const ram = [                    /* produkt 3 */
         id: 0,
         name:"ADATA Gammix D10, DDR4, 16 GB, 3200MHz, CL16",
         price: 289,
-        producent:"Adata"
+        producent:"Adata",
+        img: "../img/ram0.png"
     },
     {
         id: 1,
         name:"GoodRam IRDM, DDR4, 16 GB, 3600MHz, CL17",
         price: 319,
-        producent:"GoodRam"
+        producent:"GoodRam",
+        img: "../img/ram1.png"
     },
     {
         id: 2,
         name:"Crucial Ballistix Black at DDR4 3000 DRAM Desktop Gaming Memory Kit 16GB (8GBx2) CL15",
         price: 299,
-        producent:"Ballistix"
+        producent:"Ballistix",
+        img: "../img/ram2.png"
     },
 ]
 const psu = [                    /* produkt 4 */
@@ -87,19 +95,22 @@ const psu = [                    /* produkt 4 */
         id: 0,
         name:"SilentiumPC Vero L3 500W",
         price: 219,
-        producent:"Silentium"
+        producent:"Silentium",
+        img: "../img/psu0.png"
     },
     {
         id: 1,
         name:"be quiet! SYSTEM POWER 9 500W",
         price: 237,
-        producent:"be quiet!"
+        producent:"be quiet!",
+        img: "../img/psu1.png"
     },
     {
         id: 2,
         name:"Thermaltake Smart BM1 600W",
         price: 289,
-        producent:"Thermaltake"
+        producent:"Thermaltake",
+        img: "../img/psu2.png"
     },
 ]
 var cart = [
@@ -145,27 +156,27 @@ function SelectedItemValue(SelectId){
     const summaryPSU = document.getElementById("summaryPSU");
 
     if (SelectId == "mb") {
-        summaryMB.innerHTML = "<td><img src='img/"+mb[strAtt].img+"'></td><td>"+mb[strAtt-1].name+"</td><td>"+mb[strAtt-1].price+"</td>"
+        summaryMB.innerHTML = "<td><img src='img/"+mb[strAtt].img+"'></td><td>"+mb[strAtt].name+"</td><td>"+mb[strAtt].price+"</td>"
         cart[0].price = mb[strAtt].price
         cart[0].name = mb[strAtt].name
     }
     else if (SelectId == "cpu") {
-        summaryCPU.innerHTML = "<td><img src='img/"+cpu[strAtt].img+"'></td><td>"+cpu[strAtt-1].name+"</td><td>"+cpu[strAtt-1].price+"</td>"
+        summaryCPU.innerHTML = "<td><img src='img/"+cpu[strAtt].img+"'></td><td>"+cpu[strAtt].name+"</td><td>"+cpu[strAtt].price+"</td>"
         cart[1].price = cpu[strAtt].price
         cart[1].name = cpu[strAtt].name
     }
     else if (SelectId == "gpu") {
-        summaryGPU.innerHTML = "<td><img src='img/"+gpu[strAtt].img+"'></td><td>"+gpu[strAtt-1].name+"</td><td>"+gpu[strAtt-1].price+"</td>"
+        summaryGPU.innerHTML = "<td><img src='img/"+gpu[strAtt].img+"'></td><td>"+gpu[strAtt].name+"</td><td>"+gpu[strAtt].price+"</td>"
         cart[2].price = gpu[strAtt].price
         cart[2].name = gpu[strAtt].name
     }
     else if (SelectId == "ram") {
-        summaryRAM.innerHTML = "<td><img src='img/"+ram[strAtt].img+"'></td><td>"+ram[strAtt-1].name+"</td><td>"+ram[strAtt-1].price+"</td>"
+        summaryRAM.innerHTML = "<td><img src='img/"+ram[strAtt].img+"'></td><td>"+ram[strAtt].name+"</td><td>"+ram[strAtt].price+"</td>"
         cart[3].price = ram[strAtt].price
         cart[3].name = ram[strAtt].name
     }
     else if (SelectId == "psu") {
-        summaryPSU.innerHTML = "<td><img src='img/"+psu[strAtt].img+"'></td><td>"+psu[strAtt-1].name+"</td><td>"+psu[strAtt-1].price+"</td>"
+        summaryPSU.innerHTML = "<td><img src='img/"+psu[strAtt].img+"'></td><td>"+psu[strAtt].name+"</td><td>"+psu[strAtt].price+"</td>"
         cart[4].price = psu[strAtt].price
         cart[4].name = psu[strAtt].name
     }
